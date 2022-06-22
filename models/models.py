18636140ebf0d74cccb5edc85a0ebf16ef234b58
Sergey Malashenko <sergey.malashenko@erlyvideo.org>
2022-06-22 12:40:53 +0300

    Initial update

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
-from torch import nn
 from torchvision import models
+from torch       import nn
 
 
 def load_model(config):
@@ -10,10 +10,34 @@ def load_model(config):
     """
     arch = config.model.arch
     num_classes = config.dataset.num_of_classes
-    if arch.startswith('resnet'):
-        model = models.__dict__[arch](pretrained=True)
-        model.fc = nn.Linear(model.fc.in_features, num_classes)
-    else:
-        raise Exception('model type is not supported:', arch)
+    
+    model = models.__dict__[arch](pretrained=True)
+    
+    if 'classifier' in model._modules.keys():
+        model._modules['classifier'] 
+        if isinstance( model._modules['classifier'], nn.Linear):
+            in_features  = model._modules['classifier'].in_features
+            out_features = num_classes
+            model._modules['classifier'] = nn.Linear(in_features, out_features)
+            
+            nn.init.uniform_( model._modules['classifier'].weight , a=0.0, b=1.0)
+            nn.init.uniform_( model._modules['classifier'].bias   , a=0.0, b=1.0)
+        if isinstance( model._modules['classifier'], nn.Sequential):
+            in_features  = model._modules['classifier'][-1].in_features
+            out_features = num_classes
+            model._modules['classifier'][-1] = nn.Linear(in_features, out_features)
+            
+            nn.init.uniform_( model._modules['classifier'][-1].weight , a=0.0, b=1.0)
+            nn.init.uniform_( model._modules['classifier'][-1].bias   , a=0.0, b=1.0)
+            
+    elif 'fc' in model._modules.keys():
+        in_features  = model._modules['fc'].in_features
+        out_features = num_classes
+        
+        model._modules['fc'] = nn.Linear(in_features, out_features)
+        
+        nn.init.uniform_(model._modules['fc'].weight , a=0.0, b=1.0)
+        nn.init.uniform_(model._modules['fc'].bias   , a=0.0, b=1.0)
+    
     model.to('cuda')
     return model
---
+++ train_all_models.sh
@@ -0,0 +1,35 @@
+date
+python main.py --cfg config/baseline_mcs_resnet18.yml >baseline_mcs_resnet18.out 
+date
+python main.py --cfg config/baseline_mcs_resnet50.yml >baseline_mcs_resnet50.out
+date
+python main.py --cfg config/baseline_mcs_resent152.yml >baseline_mcs_resent152.out
+
+date
+python main.py --cfg config/baseline_mcs_densenet121.yml >baseline_mcs_densenet121.out
+date
+python main.py --cfg config/baseline_mcs_densenet161.yml >baseline_mcs_densenet161.out
+date
+python main.py --cfg config/baseline_mcs_densenet169.yml >baseline_mcs_densenet169.out
+
+date
+python main.py --cfg config/baseline_mcs_efficient_b0.yml >baseline_mcs_efficient_b0.out 
+date
+python main.py --cfg config/baseline_mcs_efficient_b1.yml >baseline_mcs_efficient_b1.out 
+date
+python main.py --cfg config/baseline_mcs_efficient_b2.yml >baseline_mcs_efficient_b2.out 
+
+date
+python main.py --cfg config/baseline_mcs_efficient_b3.yml >baseline_mcs_efficient_b3.out 
+date
+python main.py --cfg config/baseline_mcs_efficient_b4.yml >baseline_mcs_efficient_b4.out 
+date
+python main.py --cfg config/baseline_mcs_efficient_b5.yml >baseline_mcs_efficient_b5.out 
+date
+python main.py --cfg config/baseline_mcs_efficient_b6.yml >baseline_mcs_efficient_b6.out 
+date
+python main.py --cfg config/baseline_mcs_efficient_b7.yml >baseline_mcs_efficient_b7.out 
+
+
+date
+python main.py --cfg config/baseline_mcs_inception_v3.yml >baseline_mcs_inception_v3.out
